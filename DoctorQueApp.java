7603da60b380dce1920fd6b9af6a23c86310c1e8
Nathan Andrews <nathoandrews@gmail.com>
2021-10-22 19:36:53 +0100

    App class where program is run

---
+++ DoctorQueApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doctorqueapp;
+
+/**
+ *
+ * @author Nathan
+ */
+public class DoctorQueApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Running the application from here, or also doable inside the GUI app by pressing F6
+        DocGUI app = new DocGUI();
+
+        app.setVisible(true);
+    }
+
+}
